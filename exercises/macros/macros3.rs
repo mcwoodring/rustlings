65d557304a08d72f42c1ec88be1b7b2c25a870e3
Mike Woodring <mike.woodring@gmail.com>
2022-10-22 18:39:45 -0600

    Through /macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,15 +2,26 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
 }
+
+/*
+    The 'use macros::*' directive below works, but I'm not sure whether it is the intended solution.
+
+    Per the "hint" for macros3.rs:
+        In order to use a macro outside of its module, you need to do something
+        special to the module to lift the macro out into its parent.
+
+        The same trick also works on "extern crate" statements for crates that have
+        exported macros, if you've seen any of those around.
+*/
+use macros::*;
 
 fn main() {
     my_macro!();
